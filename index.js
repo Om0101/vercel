c58eef483e452acba8b11f73a5940c069d4ec380
om <omnakranif2507@gmail.com>
2025-01-17 16:48:22 +0530

    update

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
 const path = require('path');
-const characterRouter = require('./routes/starRouter'); // Assuming this is where the API logic is
+const characterRouter = require('./routes/starRouter')
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-// Serve static files from the 'build' directory
 app.use(express.static(path.join(__dirname, 'build')));
+app.use('/api',characterRouter.router)
 
-// API Routes
-app.use('/api', characterRouter.router);
 
-// Serve the front-end application (index.html) for any non-API routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 9000
 app.listen(port, () => {
-  console.log('Server started');
-});
+    console.log("server started");
+  });
