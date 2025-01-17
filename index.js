06ed38e4044fc4b9ddca62c034a6d094116be3ac
om <omnakranif2507@gmail.com>
2025-01-17 18:08:19 +0530

    update

--- index.js
+++ index.js
@@ -1,43 +1,18 @@
-// const express = require('express')
-// const cors = require('cors')
-// require('dotenv').config()
-// const path = require('path');
-// const characterRouter = require('./routes/starRouter')
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.static(path.join(__dirname, 'build')));
-// app.use('/api',characterRouter.router)
-
-
-// const port = process.env.PORT || 9000
-// app.listen(port, () => {
-//     console.log("server started");
-//  });
-
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
 const path = require('path');
-const characterRouter = require('./routes/starRouter');
+const characterRouter = require('./routes/starRouter')
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
+app.use('/api',characterRouter.router)
 
-// API routes
-app.use('/api', characterRouter.router);
 
-// Serve index.html for all non-API routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 9000
 app.listen(port, () => {
-  console.log("Server started");
-});
+    console.log("server started");
+ });
