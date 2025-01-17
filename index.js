1bb32d3fa72d8861a9dc01468310de9f6e9b5989
om <omnakranif2507@gmail.com>
2025-01-17 16:53:20 +0530

    update

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ app.use('/api',characterRouter.router)
 
 
 const port = process.env.PORT || 9000
-app.listen(port, () => {
+app.listen(port,'0.0.0.0', () => {
     console.log("server started");
   });
